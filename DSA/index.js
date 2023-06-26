84194a1d992aa8cde332a1dd72d3b5d6f2c4ac56
Precious Nnam <nnamvannessa@gmail.com>
2023-06-26 21:52:27 +0100

    Build Array from permutation and Concatenating Arrays

--- DSA/index.js
+++ DSA/index.js
@@ -22,3 +22,33 @@ const fibonacciRecursive = (n) => {
 };
 
 console.log(fibonacciRecursive(10));
+
+//Build Array from Permutation
+const BuildArr = (nums) => {
+  let ans = [];
+  for (let i = 0; i < nums.length; i++) {
+    ans[i] = nums[nums[i]];
+  }
+  return ans;
+};
+
+let nums = [5, 0, 1, 2, 3, 4];
+console.log(BuildArr(nums));
+
+// Given an integer array nums of length n, you want to create an array ans of length 2n where ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
+// Specifically, ans is the concatenation of two nums arrays.
+const getConcatenation = (nums) => {
+  let ans = nums;
+  const n = nums.length;
+  for (let i = 0; i < n; i++) {
+    ans.push(nums[i]);
+  }
+  return ans;
+};
+
+const getConcatenation1 = function (nums) {
+  nums.push(...nums);
+  return nums;
+};
+
+conxsole.log(getConcatenation(nums));
