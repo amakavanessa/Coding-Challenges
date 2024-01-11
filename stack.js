3ad15546aa90ffdb12ef616f5063fae1f2490757
Precious Nnam <nnamvannessa@gmail.com>
2024-01-11 13:31:28 +0100

    implementing stacks using arrays

--- stack.js
+++ stack.js
@@ -46,10 +46,42 @@ const myStack = new Stack();
 myStack.push("amazon");
 myStack.push("meta");
 myStack.push("google");
-console.log(myStack.peek());
-myStack.pop();
-myStack.pop();
-myStack.pop();
 // console.log(myStack.peek());
+myStack.pop();
+myStack.pop();
+myStack.pop();
+console.log(myStack.peek());
 console.log(myStack.isEmpty());
 console.log(myStack);
+
+//implement stacks with Arrays
+class StackArr {
+  constructor() {
+    this.stack = [];
+  }
+
+  peek() {
+    if (this.stack.length) {
+      return this.stack[this.stack.length - 1];
+    }
+    return "empty stack!";
+  }
+
+  push(value) {
+    this.stack.push(value);
+  }
+
+  pop() {
+    this.stack.pop();
+  }
+}
+
+const stack2 = new StackArr();
+
+stack2.push("google");
+stack2.push("udemy");
+stack2.push("discord");
+stack2.pop();
+
+console.log(stack2.peek());
+console.log(stack2);
