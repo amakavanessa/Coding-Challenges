d0270b6fd7962a5fc49e02fd91268f72c00b9e47
Precious Nnam <nnamvannessa@gmail.com>
2024-01-22 11:25:06 +0100

    implememnting select sort algorithm

--- bubblesort.js
+++ bubblesort.js
@@ -10,22 +10,7 @@ function bubbleSort(array) {
     }
   }
 }
-// function bubbleSort(array) {
-//   const length = array.length;
-//   for (let i = 0; i < length; i++) {
-//     for (let j = 0; j < length; j++) {
-//       if (array[j] > array[j + 1]) {
-//         //Swap the numbers
-//         let temp = array[j];
-//         array[j] = array[j + 1];
-//         array[j + 1] = temp;
-//       }
-//     }
-//   }
-// }
 
 const numbers = [6, 5, 3, 1, 8, 7, 2, 4];
 bubbleSort(numbers);
 console.log(numbers);
-
-// console.log(bubbleSort([6, 5, 3, 1, 8, 7, 2, 4]));
