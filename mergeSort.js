890245fc34833e26843a31c164c4fbac384009b5
Precious Nnam <nnamvannessa@gmail.com>
2024-01-22 16:09:40 +0100

    merge sort implementation

---
+++ mergeSort.js
@@ -0,0 +1,36 @@
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function mergeSort(array) {
+  if (array.length === 1) {
+    return array;
+  }
+  //split array into right and left
+
+  const mid = Math.floor(array.length / 2);
+  let left = array.slice(0, mid);
+  let right = array.slice(mid, array.length);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+  let sortedArr = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      sortedArr.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      sortedArr.push(right[rightIndex]);
+      ri ghtIndex++;
+    }
+  }
+  return sortedArr
+    .concat(left.slice(leftIndex))
+    .concat(right.slice(rightIndex));
+}
+
+const answer = mergeSort(numbers);
+console.log(answer);
